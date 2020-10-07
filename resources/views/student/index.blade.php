8fe08e050b89caa28121992874f4a5ef337ff59e
9ec_100007489776_duanyc <9ec_100007489776_duanyc@git.cloud.tencent.com>
2020-10-08 01:19:16 +0800

    完成学生列表的模版

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -3,3 +3,67 @@
 @section('title')
 首页
 @stop
+
+@section('content')
+    <div style="padding: 15px;">
+        学生列表
+        <table class="layui-table">
+            <colgroup>
+                <col width="100">
+                <col width="200">
+                <col width="100">
+                <col width="100">
+                <col width="200">
+                <col>
+            </colgroup>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>姓名</th>
+                <th>年龄</th>
+                <th>性别</th>
+                <th>添加时间</th>
+                <th>操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>张三</td>
+                <td>12</td>
+                <td>男</td>
+                <td>2016-11-29</td>
+                <td>
+                    <button type="button" class="layui-btn layui-btn-sm layui-btn-primary">
+                        <i class="layui-icon">&#xe63c;</i>
+                    </button>
+                    <button type="button" class="layui-btn layui-btn-sm layui-btn-primary">
+                        <i class="layui-icon">&#xe642;</i>
+                    </button>
+                    <button type="button" class="layui-btn layui-btn-sm layui-btn-primary">
+                        <i class="layui-icon">&#xe640;</i>
+                    </button>
+                </td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>李四</td>
+                <td>18</td>
+                <td>女</td>
+                <td>2019-11-29</td>
+                <td>
+                    <button type="button" class="layui-btn layui-btn-sm layui-btn-primary">
+                        <i class="layui-icon">&#xe63c;</i>
+                    </button>
+                    <button type="button" class="layui-btn layui-btn-sm layui-btn-primary">
+                        <i class="layui-icon">&#xe642;</i>
+                    </button>
+                    <button type="button" class="layui-btn layui-btn-sm layui-btn-primary">
+                        <i class="layui-icon">&#xe640;</i>
+                    </button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+@stop
